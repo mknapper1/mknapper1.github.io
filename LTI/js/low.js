fbbc415e3ea1520d9e8e9e8782cd9d1c81984e8b
Matthew Knapper <mattknapper@lllow.com>
2017-01-11 14:04:47 -0500

    added about and contact form

--- LTI/js/low.js
+++ LTI/js/low.js
@@ -1,26 +1,34 @@
 clients = [
-    {name: 'Allen County', img: 'images/counties/allen.jpg', url:'http://lowtaxinfo.com/allencounty'},
-    //Add additional counties between here!!!
-     {name: 'Allen County', img: 'images/counties/allen.jpg', url:'http://lowtaxinfo.com/allencounty'},
-     {name: 'Allen County', img: 'images/counties/allen.jpg', url:'http://lowtaxinfo.com/allencounty'},
-     {name: 'Allen County', img: 'images/counties/allen.jpg', url:'http://lowtaxinfo.com/allencounty'},
+    {
+        name: 'Allen County'
+        , img: 'images/counties/allen.jpg'
+        , url: 'http://lowtaxinfo.com/allencounty'
+    }
+    , //Add additional counties between here!!!
+//     {name: 'Allen County', img: 'images/counties/allen.jpg', url:'http://lowtaxinfo.com/allencounty'},
+//     {name: 'Allen County', img: 'images/counties/allen.jpg', url:'http://lowtaxinfo.com/allencounty'},
+//     {name: 'Allen County', img: 'images/counties/allen.jpg', url:'http://lowtaxinfo.com/allencounty'},
     //And here!
-    {name: 'More Coming Soon', img: 'images/counties/more.jpg', url:null},
-]
-    
+    {
+        name: 'More Coming Soon'
+        , img: 'images/counties/more.jpg'
+        , url: null
+    }
+, ]
+
 
 counties = {
-    create: function(client) {
+    create: function (client) {
         var div = document.createElement('div');
         if (client.url != null) {
             div.setAttribute('class', 'col-sm-3 col-sm-pull-1 col-sm-offset-1 pointer');
-            div.onclick = function() {
-                location.replace(client.url);            
-            };    
+            div.onclick = function () {
+                location.replace(client.url);
+            };
         } else {
-        div.setAttribute('class', 'col-sm-3 col-sm-pull-1 col-sm-offset-1');
+            div.setAttribute('class', 'col-sm-3 col-sm-pull-1 col-sm-offset-1');
         }
-        
+
         var img = document.createElement('img');
         img.setAttribute('src', client.img);
         img.setAttribute('class', 'img-circle county-img');
@@ -28,22 +36,22 @@ counties = {
         var h3 = document.createElement('h3');
         h3.innerHTML = client.name;
 
-        div.appendChild(img);        
+        div.appendChild(img);
         div.appendChild(h3);
 
         return div;
-    },
-    init: {
-        all: function() {
+    }
+    , init: {
+        all: function () {
             var client_container = document.getElementById('clients_container');
-            for(var i = 0; i < clients.length; i++) {
+            for (var i = 0; i < clients.length; i++) {
                 client_container.appendChild(counties.create(clients[i]));
             }
-            
+
         }
     }
 }
 
-$(function() {
+$(function () {
     counties.init.all();
 });
